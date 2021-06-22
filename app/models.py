b51529bb7f7ff99f2bf7bbbb3181274bd7b7502d
juma-moringa <allan.juma@student.moringaschool.com>
2021-06-22 16:30:04 +0300

    defined the verify password function in the user class

--- app/models.py
+++ app/models.py
@@ -27,3 +27,18 @@ class User(UserMixin,db.Model):
     password_hash = db.Column(db.String(255))
 
 
+    
+    @property
+    def password(self):
+        raise AttributeError('You cannnot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
+    def __repr__(self):
+        return f'User {self.username}'
