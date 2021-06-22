b5ae41f4b5d9446f2fb9e3ebb2cc700bca12446f
Dennis <dennis.kimani@student.moringaschool.com>
2021-06-22 22:31:16 +0300

    navbar update

--- app/main/views.py
+++ app/main/views.py
@@ -11,6 +11,6 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    title = 'Home - Welcome to The best Movie Review Website Online'
+    title = 'Home -Welcha, to mech hub'
 
     return render_template('index.html', title=title )
