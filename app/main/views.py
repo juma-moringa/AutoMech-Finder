37af70f99c49e4fcc09977e1f084506dcc398334
juma-moringa <allan.juma@student.moringaschool.com>
2021-06-22 15:11:25 +0300

    extended the base html to the index page

---
+++ app/main/views.py
@@ -0,0 +1,22 @@
+from flask import render_template, request, redirect, url_for
+from flask.helpers import flash
+from . import main
+from .. import db
+from ..models import User
+from flask_login import login_required, current_user
+from datetime import datetime
+# from ..request import get_quote
+from ..email import mail_message
+
+
+# 1 the main index(default)
+@main.route("/", methods=["GET", "POST"])
+def index():
+
+        # db.session.add()
+        # db.session.commit()
+        # mail_message("Thank you for subscribing to Automech",
+        #              "email/welcome")
+
+        return render_template("index.html")
+
