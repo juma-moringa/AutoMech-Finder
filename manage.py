9100ecfbd657d87d6d72c30a3aad99463eaaab8b
juma-moringa <allan.juma@student.moringaschool.com>
2021-06-23 21:59:40 +0300

    updates

--- manage.py
+++ manage.py
@@ -7,7 +7,8 @@ from  flask_migrate import Migrate,MigrateCommand
 # Creating app instances
 #1
 app = create_app('development')
-
+#2
+# app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server',Server)
