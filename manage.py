14051bac477d231805bee91b860e67ffd03603f2
jLuseno161 <joyluseno2@gmail.com>
2021-06-23 21:22:41 +0300

    edit manage.py

--- manage.py
+++ manage.py
@@ -1,17 +1,23 @@
-from app import create_app,db
-from flask_script import Manager,Server
+from app import create_app, db
+from flask_script import Manager, Server
 from app.models import User
-from  flask_migrate import Migrate,MigrateCommand
+from flask_migrate import Migrate, MigrateCommand
 
 
+# Creating app instances
+# 1
 app = create_app('development')
+# 2
+# app = create_app('production')
 
 manager = Manager(app)
-manager.add_command('server',Server)
+manager.add_command('server', Server)
 
-manager.add_command('db',MigrateCommand)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+
 @manager.command
-
 def test():
     """Run the unit tests."""
     import unittest
@@ -21,7 +27,8 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User)
+    return dict(app=app, db=db, User=User)
+
 
 if __name__ == '__main__':
     manager.run()
